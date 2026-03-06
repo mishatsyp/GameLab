99c0a9a96b99d4524e1b72f9565f1e785ebc4fc7
stumanian <ss.tumanian@mail.ru>
2026-03-06 18:29:32 +0300

    room.cpp
    implemented room.cpp. created almost all methods

---
+++ src/Room.cpp
@@ -0,0 +1,64 @@
+#include "../include/Room.h"
+#include "../include/Player.h"
+#include "../include/Event.h"
+#include <memory>
+#include <string>
+
+// Конструктор
+Room::Room(RoomType roomType)
+    : type(roomType)
+    , isExplored(false) {
+
+    // Если комната с событием, создаем событие
+    if (type == RoomType::EVENT) {
+        roomEvent = std::make_unique<Event>();
+    }
+}
+
+// Генерация контента (пока ничего не делает)
+void Room::generateRoomContent() {
+
+}
+
+// Вход в комнату
+std::string Room::enter(Player& player) {
+    if (!isExplored) {
+        isExplored = true;
+
+        if (type == RoomType::EMPTY) {
+            return "Вы вошли в пустую комнату.";
+        }
+        else if (type == RoomType::EVENT) {
+            return "Вы вошли в комнату с событием!";
+        }
+        else if (type == RoomType::EXIT) {
+            return "Вы нашли выход!";
+        }
+    }
+а
+    return "Вы снова в этой комнате.";
+}
+
+// Осмотр комнаты
+std::string Room::look() const {
+    if (type == RoomType::EMPTY) {
+        return "Пустая комната.";
+    }
+    else if (type == RoomType::EVENT) {
+        if (roomEvent) {
+            return roomEvent->getDescription();
+        }
+        return "Здесь что-то происходит...";
+    }
+    else {
+        return "Здесь выход!";
+    }
+}
+
+// Проверка активного события
+bool Room::hasActiveEvent() const {
+    if (roomEvent) {
+        return !roomEvent->getIsCompleted();
+    }
+    return false;
+}
